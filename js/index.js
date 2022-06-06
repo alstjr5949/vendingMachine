5a7735f714f1b2d72827e90f67d8e4a370d5d0fe
alstjr5949 <alstjr5949@gmail.com>
2022-06-06 22:19:32 +0900

    입금, 거스름돈 버튼 js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const balanceMoney = document.querySelector(".balanceMoney");
 const depositForm = document.querySelector(".depositForm");
 
+// 소지금 입금
 depositForm.addEventListener("submit", (e) => {
   const dimBox = document.querySelector(".dim");
   const moneyModal = document.querySelector(".moneyModal");
@@ -14,5 +15,45 @@ depositForm.addEventListener("submit", (e) => {
     balanceMoney.innerText = `${depositMoney.toLocaleString("ko-KR")} 원`;
   } else {
     e.target[0].style.outline = "1px solid red";
+  }
+});
+
+// 잔액 입금
+const depositBtn = document.querySelector(".depositBtn");
+
+depositBtn.addEventListener("click", () => {
+  const depositInput = document.querySelector(".depositInput");
+  const balanceMoneyLeft = document.querySelector(".balanceMoneyLeft");
+  const myMoney =
+    balanceMoney.innerText.slice(0, 2) + balanceMoney.innerText.slice(3, 6);
+  const depositMoney = +depositInput.value;
+  if (depositMoney > myMoney) {
+    alert("소지금을 확인해보세요.");
+  } else {
+    balanceMoneyLeft.innerText = `${depositMoney.toLocaleString("ko-KR")} 원`;
+    balanceMoney.innerText = `${(myMoney - depositMoney).toLocaleString(
+      "ko-KR"
+    )} 원`;
+    depositInput.value = "";
+  }
+});
+
+// 거스름돈 반환
+const changeBtn = document.querySelector(".changeBtn");
+
+changeBtn.addEventListener("click", () => {
+  const balanceMoneyLeft = document.querySelector(".balanceMoneyLeft");
+  const balanceMoneyValue =
+    balanceMoneyLeft.innerText.slice(0, 2) +
+    balanceMoneyLeft.innerText.slice(3, 6);
+  const myMoney =
+    balanceMoney.innerText.slice(0, 2) + balanceMoney.innerText.slice(3, 6);
+  if (balanceMoneyValue === "원") {
+    alert("거스름돈이 있는지 확인하세요.");
+  } else {
+    balanceMoney.innerText = `${(+balanceMoneyValue + +myMoney).toLocaleString(
+      "ko-KR"
+    )} 원`;
+    balanceMoneyLeft.innerText = " 원";
   }
 });
