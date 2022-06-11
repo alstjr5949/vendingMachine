60b019c1bbca263d23d0df4ab74b478c4cdce8ff
alstjr5949 <alstjr5949@gmail.com>
2022-06-11 16:03:24 +0900

    first version vending machine

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
+let colaData = [
+  {
+    colaName: "Original_Cola",
+    stock: 3,
+  },
+  {
+    colaName: "Violet_Cola",
+    stock: 3,
+  },
+  {
+    colaName: "Yellow_Cola",
+    stock: 3,
+  },
+  {
+    colaName: "Cool_Cola",
+    stock: 3,
+  },
+  {
+    colaName: "Green_Cola",
+    stock: 3,
+  },
+  {
+    colaName: "Orange_Cola",
+    stock: 3,
+  },
+];
+
 const balanceMoney = document.querySelector(".myMoneyNum");
 const depositForm = document.querySelector(".depositForm");
 
@@ -8,6 +35,23 @@ const commaGenerator = (num) => {
 
 const commaDelete = (string) => {
   return string.replace(/\,/g, "");
+};
+
+const soldOutCheck = (list, colaName, clickBtn) => {
+  colaData.forEach((cola) => {
+    console.log(cola.stock);
+    if (cola.colaName == colaName) {
+      cola.stock--;
+      if (cola.stock == 0) {
+        clickBtn.setAttribute("disabled", "disabled");
+        const soldOutImg = document.createElement("img");
+        soldOutImg.setAttribute("class", "soldOut");
+        soldOutImg.setAttribute("src", "./imgs/품절 표시.png");
+        soldOutImg.setAttribute("alt", "품절");
+        list.append(soldOutImg);
+      }
+    }
+  });
 };
 
 // 소지금 입금
@@ -63,7 +107,7 @@ const purchaseBtn = document.querySelectorAll(".purchaseBtn");
 
 let cartDrinkName = [];
 // 왼쪽 아래 음료 리스트 생성 함수
-const createDrink = (colaImg, colaName) => {
+const createDrink = (list, colaImg, colaName, clickBtn) => {
   const getDrinkListBox = document.querySelector(".getListBox");
   const drinkList = document.createElement("li");
   drinkList.classList.add("getList");
@@ -81,6 +125,7 @@ const createDrink = (colaImg, colaName) => {
   drinkList.appendChild(drinkName);
   drinkList.appendChild(drinkCount);
   getDrinkListBox.appendChild(drinkList);
+  soldOutCheck(list, colaName, clickBtn);
 };
 
 //중복 체크
@@ -91,6 +136,7 @@ purchaseBtn.forEach((purchaseBtn) => {
     let balanceMoneyValue = +commaDelete(balanceMoneyNum.innerText);
     const colaPrice = 1000;
     if (balanceMoneyValue >= 1000) {
+      const clickBtn = e.target;
       const list = e.target.parentNode;
       const img = list.childNodes[1].src;
       const colaName = list.childNodes[3].innerText;
@@ -99,6 +145,8 @@ purchaseBtn.forEach((purchaseBtn) => {
         if (drink.colaName === colaName) {
           const drinkGetList = document.querySelectorAll(".getListLeft");
           drink.count += 1;
+          soldOutCheck(list, colaName, clickBtn);
+
           checkFlag = true;
           drinkGetList.forEach((item) => {
             if (item.childNodes[1].innerText == drink.colaName) {
@@ -107,11 +155,11 @@ purchaseBtn.forEach((purchaseBtn) => {
           });
         }
       });
+
       if (checkFlag === false) {
-        createDrink(img, colaName);
+        createDrink(list, img, colaName, clickBtn);
         cartDrinkName.push({ colaName, count: 1 });
       }
-      // 문제
       balanceMoneyNum.innerText = `${(
         balanceMoneyValue - colaPrice
       ).toLocaleString("ko-KR")}`;
@@ -119,4 +167,30 @@ purchaseBtn.forEach((purchaseBtn) => {
       alert("잔액을 확인하세요.");
     }
   });
+});
+
+//획득 버튼 클릭
+const getBtn = document.querySelector(".getBtn");
+
+getBtn.addEventListener("click", () => {
+  const getListLeft = document.querySelectorAll(".getListLeft");
+  const myBeverageListBox = document.querySelector(".myBeverageListBox");
+  const totalMoney = document.querySelector(".totalMoney");
+  getListLeft.forEach((listLeft) => {
+    listLeft.classList.remove("getListLeft");
+  });
+  myBeverageListBox.append(...getListLeft);
+  let myBeverageListCheck = document.querySelectorAll(
+    ".myBeverageListBox .getList"
+  );
+  cartDrinkName = [];
+  let myBeverageNum = Array.from(myBeverageListCheck).map(
+    (i) => i.childNodes[2].innerText
+  );
+
+  let myBeverageNumTotal = myBeverageNum.reduce((cur, prev) => {
+    return +cur + +prev;
+  });
+
+  totalMoney.innerText = myBeverageNumTotal * 1000;
 });
